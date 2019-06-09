44cfd752bba7c3b8785ce1f89564af61576acc92
Cedric Beust <cedric@beust.com>
2019-06-09 12:53:35 -0700

    Parens in the background.

--- src/main/kotlin/com/beust/kash/Command.kt
+++ src/main/kotlin/com/beust/kash/Command.kt
@@ -1,29 +1,29 @@
 package com.beust.kash
 
 
-sealed class Command {
+sealed class Command(open val background: Boolean) {
     val tokens: List<Token>
         get() = when(this) {
             is SingleCommand -> this.exec.tokens
-            is AndCommands -> this.execs[0].tokens
-            is PipeCommands -> this.execs[0].tokens
+            is AndCommands -> this.execs.flatMap { it.tokens }
+            is PipeCommands -> this.execs.flatMap { it.tokens }
             is ParenCommand -> this.command.tokens
         }
 
     val words: List<String>
         get() = when(this) {
             is SingleCommand -> this.exec.words
-            is AndCommands -> this.execs[0].words
-            is PipeCommands -> this.execs[0].words
+            is AndCommands -> this.execs.flatMap { it.words }
+            is PipeCommands -> this.execs.flatMap { it.words }
             is ParenCommand -> this.command.words
         }
 
     val firstWord: String
         get() = words[0]
 
-    data class SingleCommand(val exec: Exec) : Command()
-    data class AndCommands(val execs: List<Exec>) : Command()
-    data class PipeCommands(val execs: List<Exec>): Command()
-    data class ParenCommand(val command: Command): Command()
+    data class SingleCommand(val exec: Exec, override val background: Boolean) : Command(background)
+    data class AndCommands(val execs: List<Exec>, override val background: Boolean) : Command(background)
+    data class PipeCommands(val execs: List<Exec>, override val background: Boolean): Command(background)
+    data class ParenCommand(val command: Command, override val background: Boolean): Command(background)
 }
 
