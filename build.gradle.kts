af6d06e9adc843664708383286bc7c42603f9c75
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2019-06-13 14:59:23 +0300

    Switch to Kotlin 1.3.40 (eap-105)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 buildscript {
-    val kotlinVer by extra { "1.3.31" }
+    val kotlinVer by extra { "1.3.40-eap-105" }
 
     repositories {
         jcenter()
@@ -19,20 +19,23 @@ repositories {
     jcenter()
     mavenCentral()
     maven { setUrl("https://plugins.gradle.org/m2") }
+    maven { setUrl("https://dl.bintray.com/kotlin/kotlin-eap") }
 }
 
 plugins {
     java
     application
     idea
-    id("org.jetbrains.kotlin.jvm") version "1.3.31"
+    id("org.jetbrains.kotlin.jvm") version "1.3.40-eap-105"
     id("com.github.johnrengelman.shadow") version "4.0.2"
 }
 
+val kotlinVer by extra { "1.3.40-eap-105" }
+
 dependencies {
-    listOf("org.jetbrains.kotlin:kotlin-compiler-embeddable:1.3.31",
-            "org.jetbrains.kotlin:kotlin-scripting-compiler-embeddable:1.3.31",
-            "org.jetbrains.kotlin:kotlin-script-util:1.3.31",
+    listOf("org.jetbrains.kotlin:kotlin-compiler-embeddable:$kotlinVer",
+            "org.jetbrains.kotlin:kotlin-scripting-compiler-embeddable:$kotlinVer",
+            "org.jetbrains.kotlin:kotlin-script-util:$kotlinVer",
             "org.jline:jline:3.11.0",
             "org.fusesource:fuse-project:7.2.0.redhat-060",
             "org.slf4j:slf4j-api:1.8.0-beta4",
