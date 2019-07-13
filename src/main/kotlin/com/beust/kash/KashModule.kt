020ab02b64c70428d966a926f6e1ba1dc7d560bc
Cedric Beust <cedric@beust.com>
2019-07-13 11:10:10 -0700

    Injection refactoring.

--- src/main/kotlin/com/beust/kash/KashModule.kt
+++ src/main/kotlin/com/beust/kash/KashModule.kt
@@ -1,24 +1,24 @@
 package com.beust.kash
 
 import com.google.inject.AbstractModule
+import com.google.inject.Inject
+import com.google.inject.Provider
+import com.google.inject.Singleton
 import org.jline.terminal.Terminal
 import org.jline.terminal.TerminalBuilder
-import org.slf4j.LoggerFactory
-import java.io.InputStreamReader
 import javax.script.ScriptEngine
 import javax.script.ScriptEngineManager
 
-class KashModule() : AbstractModule() {
-    private val log = LoggerFactory.getLogger(KashModule::class.java)
+class ScriptEngineProvider @Inject constructor(): Provider<ScriptEngine> {
+    override fun get(): ScriptEngine {
+        val result = ScriptEngineManager().getEngineByExtension("kash.kts")
+        return result ?: throw IllegalArgumentException("Couldn't find a script engine for .kash.kts")
+    }
+}
 
+class KashModule() : AbstractModule() {
     override fun configure() {
         bind(Terminal::class.java).toInstance(TerminalBuilder.builder().build())
-
-        val scriptEngine = ScriptEngineManager().getEngineByExtension("kash.kts")
-                ?: throw IllegalArgumentException("Couldn't find a script engine for .kash.kts")
-
-        val engine = Engine(scriptEngine)
-        bind(ScriptEngine::class.java).toInstance(scriptEngine)
-        bind(Engine::class.java).toInstance(engine)
+        bind(ScriptEngine::class.java).toProvider(ScriptEngineProvider::class.java).`in`(Singleton::class.java)
     }
 }
