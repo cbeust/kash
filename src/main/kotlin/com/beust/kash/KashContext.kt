fa30999ac84f57f3590b305293c8ee4edc7e0d35
Cedric Beust <cedric@beust.com>
2019-08-09 19:23:36 -0700

    Fix the directory stack.

--- src/main/kotlin/com/beust/kash/KashContext.kt
+++ src/main/kotlin/com/beust/kash/KashContext.kt
@@ -2,6 +2,8 @@ package com.beust.kash
 
 import com.beust.kash.api.IKashContext
 import com.google.inject.Inject
+import java.io.File
+import java.util.*
 
 class KashContext @Inject constructor(engine: Engine): IKashContext {
     private val kashObject = KashObject(engine)
@@ -10,5 +12,12 @@ class KashContext @Inject constructor(engine: Engine): IKashContext {
     override val paths = kashObject.paths
     override val env = kashObject.env
     override val prompt = kashObject.prompt
-    override val directoryStack = kashObject.directoryStack
+    override val directoryStack: Stack<String>
+        get() {
+            val result = kashObject.directoryStack
+            if (result.isEmpty()) {
+                result.push(File(".").absoluteFile.canonicalPath)
+            }
+            return result
+        }
 }
