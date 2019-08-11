46314c431a39d9abcaa573dad4b549c7d071ea43
Cedric Beust <cedric@beust.com>
2019-08-11 10:51:04 -0700

    Comment out internal built-in.

--- src/main/kotlin/com/beust/kash/SimpleBuiltin.kt
+++ src/main/kotlin/com/beust/kash/SimpleBuiltin.kt
@@ -4,10 +4,10 @@ import com.beust.kash.api.Builtin
 import com.beust.kash.api.CommandResult
 import com.beust.kash.api.IKashContext
 
-class SimpleBuiltin {
-    @Builtin
-    fun hello(words: List<String>, context: IKashContext): CommandResult {
-        println("Hello " + (if (words.size > 1) words[1] else "stranger"))
-        return CommandResult(0)
-    }
-}
+//class SimpleBuiltin {
+//    @Builtin
+//    fun ohello(words: List<String>, context: IKashContext): CommandResult {
+//        println("Hello " + (if (words.size > 1) words[1] else "stranger"))
+//        return CommandResult(0)
+//    }
+//}
